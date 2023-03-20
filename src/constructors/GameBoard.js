155832fbf9badf1c041bc9df18ff375e32a0f7c2
Or Rodrigez <orrodrigez1@gmail.com>
2023-03-20 17:30:29 +0200

    Add placeShip and receiveAttack methods to board

--- src/constructors/GameBoard.js
+++ src/constructors/GameBoard.js
@@ -1,10 +1,95 @@
+import Ship from "./Ship"
+
 export default function GameBoard() {
   let board = []
   let gridSize = 100
+  // initialize coordinates at 1,1
+  let x = 1
+  let y = 1
   for(let i = 0; i < gridSize; i++) {
-    let box = []
+    // make each box an object containing data
+    let box = {
+      "coordinates": [x,y],
+      "ship": null,
+      "attacked": false,
+      "available": true
+    }
+    // add object to gameBoard array
     board.push(box)
+    
+    // increment x and y coordinates
+    x++
+    if(x > 10) {
+      x = 1
+      y++
+    }
+
   }
 
-  return board
+  return {
+    get board() {
+      return board
+    },
+
+    placeShip(coords, direction, len) {
+      // check if ship stays within boundaries
+      if(direction === 'right' && coords[0] + len > 10) return 'ship exceeding boundaries'
+      if(coords[1] + len > 10) return 'ship exceeding boundaries'
+
+      let coordsCopy = [...coords]
+
+      for(let i = 0; i < len; i++) {
+        if(i !== 0) {
+          if(direction === 'right') coordsCopy[0]++
+          else coordsCopy[1] --
+        }
+        let chosenLocation = board.find(box => box.coordinates.every((coord, index) => coord === coordsCopy[index]))
+        if(!chosenLocation.available) return 'Some of the space is unavailable'
+      }
+
+      // creates the ship
+      const battleship = new Ship(len)
+
+      for(let i = 0; i < len; i++) {
+        // skips statement on first round to add the chosen coordinates
+        if(i !== 0) {
+          // continue moving in the chosen direction
+          if(direction === 'right') coords[0]++
+          else coords[1]--
+        }
+
+        // make an array containing all of the adjacent locations
+        let adjacentLocations = [
+          [coords[0] - 1, coords[1]],
+          [coords[0] + 1, coords[1]],
+          [coords[0] - 1, coords[1] + 1],
+          [coords[0] - 1, coords[1] - 1],
+          [coords[0] + 1, coords[1] - 1],
+          [coords[0] + 1, coords[1] + 1],
+          [coords[0], coords[1] + 1],
+          [coords[0], coords[1] - 1]
+        ]
+
+        let chosenLocation = board.find(box => box.coordinates.every((coord, index) => coord === coords[index]))
+        chosenLocation.ship = battleship
+        chosenLocation.available = false
+        adjacentLocations.forEach(location => {
+          let current = board.find(box => box.coordinates.every((coord, index) => coord === location[index]))
+          if(current) current.available = false
+        })
+      }
+    },
+
+    receiveAttack(coords) {
+      const chosenLocation = board.find(box => box.coordinates.every((coord, index) => coord === coords[index]))
+      if(chosenLocation === undefined) return
+
+      if(!chosenLocation.attacked) {
+        chosenLocation.attacked = true
+        if(chosenLocation.ship) {
+          chosenLocation.ship.hit()
+        }
+      }
+    }
+  }
 }
--- src/tests/GameBoard.test.js
+++ src/tests/GameBoard.test.js
@@ -7,6 +7,76 @@ describe('Game Board', () => {
   })
 
   test('Board created', () => {
-    expect(testGameBoard.length).toBe(100)
+    expect(testGameBoard.board.length).toBe(100)
+  })
+
+  test('Each board box has coordinates', () => {
+    expect(testGameBoard.board[0].coordinates).toEqual([1,1])
+    expect(testGameBoard.board[1].coordinates).toEqual([2,1])
+    expect(testGameBoard.board[2].coordinates).toEqual([3,1])
+    expect(testGameBoard.board[10].coordinates).toEqual([1,2])
+    expect(testGameBoard.board[44].coordinates).toEqual([5,5])
+    expect(testGameBoard.board[99].coordinates).toEqual([10,10])
+  })
+
+  describe('Create ship at coordinates', () => {
+    test('With right direction', () => {
+      testGameBoard.placeShip([1,1], 'right', 3)
+      expect(testGameBoard.board[0].ship.length).toEqual(3)
+      expect(testGameBoard.board[1].ship.length).toEqual(3)
+      expect(testGameBoard.board[2].ship.length).toEqual(3)
+    })
+
+    test('With up direction', () => {
+      testGameBoard.placeShip([1,2], 'up', 2)
+      expect(testGameBoard.board[0].ship.length).toEqual(2)
+      expect(testGameBoard.board[10].ship.length).toEqual(2)
+    })
+
+    test("Abort if ship exceeds boundaries", () => {
+      testGameBoard.placeShip([8,1], 'right', 4)
+      expect(testGameBoard.board[8].ship).toEqual(null)
+    })
+
+    test("Make space unavailable when creating ship", () => {
+      testGameBoard.placeShip([2,2], 'right', 2)
+      expect(testGameBoard.board[0].available).toEqual(false)
+      expect(testGameBoard.board[1].available).toEqual(false)
+      expect(testGameBoard.board[2].available).toEqual(false)
+      expect(testGameBoard.board[3].available).toEqual(false)
+      expect(testGameBoard.board[10].available).toEqual(false)
+      expect(testGameBoard.board[11].available).toEqual(false)
+      expect(testGameBoard.board[12].available).toEqual(false)
+      expect(testGameBoard.board[13].available).toEqual(false)
+      expect(testGameBoard.board[20].available).toEqual(false)
+      expect(testGameBoard.board[21].available).toEqual(false)
+      expect(testGameBoard.board[22].available).toEqual(false)
+      expect(testGameBoard.board[23].available).toEqual(false)
+
+    })
+
+    test("Abort if space isn't available", () => {
+      testGameBoard.placeShip([1,1], 'right', 4)
+      testGameBoard.placeShip([1,2], 'right', 4)
+      testGameBoard.placeShip([5,1], 'right', 4)
+      testGameBoard.placeShip([1,1], 'right', 2)
+      expect(testGameBoard.board[0].ship.length).toEqual(4)
+      expect(testGameBoard.board[10].ship).toEqual(null)
+      expect(testGameBoard.board[14].ship).toEqual(null)
+      expect(testGameBoard.board[4].ship).toEqual(null)
+    })
+  })
+
+  describe('Receive attack', () => {
+    test("Log attack at chosen location", () => {
+      testGameBoard.receiveAttack([1, 1])
+      expect(testGameBoard.board[0].attacked).toEqual(true)
+    })
+
+    test("Hit ship on chosen location", () => {
+      testGameBoard.placeShip([1,1], 'right', 3)
+      testGameBoard.receiveAttack([1, 1])
+      expect(testGameBoard.board[0].ship.hits).toEqual(1)
+    })
   })
 })
