949b5a4bf66c026ed27887e2dc33aefcfc135226
Or Rodrigez <orrodrigez1@gmail.com>
2023-03-20 19:55:30 +0200

    Start working on the player constructor The 'shoot' method isn't working properly yet

--- src/constructors/Player.js
+++ src/constructors/Player.js
@@ -1,10 +1,21 @@
 import GameBoard from "./GameBoard"
 
-export default function Player(name) {
-  this.name = name
-  this.game = new GameBoard()
+export default function Player(playerName) {
+  const name = playerName
+  const gameBoard = new GameBoard()
 
   return {
-    name
+    name,
+    get board() {
+      return gameBoard.board
+    },
+
+    placeShip(coord, direction, len) {
+      gameBoard.placeShip(coord, direction, len)
+    },
+
+    shoot(target, coord) {
+      return target.gameBoard.receiveAttack(coord)
+    }
   }
 }
--- src/constructors/tests/player.test.js
+++ src/constructors/tests/player.test.js
@@ -1,12 +1,41 @@
 import Player from "../Player"
 
 describe('Player Constructor', () => {
-  let player
+  let player1
+  let player2
   beforeEach(() => {
-    player = new Player('Player')
+    player1 = new Player('Player1')
+    player2 = new Player('Player2')
   })
 
   test('Player created', () => {
-    expect(player.name).toBe('Player')
+    expect(player1.name).toBe('Player1')
+    expect(player2.name).toBe('Player2')
+  })
+
+  describe('Player methods', () => {
+
+    test('Get board', () => {
+      expect(player1.board).not.toBe(undefined)
+    })
+
+    test('Place ship', () => {
+      player1.placeShip([1,1], 'right', 2)
+      player2.placeShip([1,1], 'right', 3)
+      expect(player1.board[0].ship.length).toEqual(2)
+      expect(player2.board[0].ship.length).toEqual(3)
+    })
+
+    test('Shoot', () => {
+      player1.placeShip([1,1], 'right', 2)
+      player2.placeShip([1,1], 'right', 2)
+      player1.shoot(player2, [1,1])
+      player1.shoot(player2, [2,1])
+      player2.shoot(player1, [1,1])
+      expect(player1.shoot(player2, [1,1])).not.toEqual(undefined)
+      expect(player1.board[0].ship.hits).toEqual(1)
+      expect(player2.board[0].ship.hits).toEqual(2)
+      expect(player2.board[0].ship.isSunk).toEqual(true)
+    })
   })
 })
